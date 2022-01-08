40b0bd0396ad9f554865a3e4c1940914d79b97e8
Tatiana <ganzhinovat@yandex.ru>
2022-01-08 13:34:59 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(num) {
+  let str = Math.abs(num).toString();
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    result = `${str[i]}${result}`;
+  }
+
+  return +result;
 }
